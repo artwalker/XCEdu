3bd8037008fd4eaa396150a8b5581b07e36854b6
wxinxings <wxinxings@icloud.com>
2020-08-31 21:00:01 +0800

    第14天代码

---
+++ scEduUI/xc-ui-pc-portal/layouts/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>这里是头</div>
+    <nuxt/>
+    <div>这里是尾</div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+<style>
+
+</style>
---
+++ scEduUI/xc-ui-pc-portal/pages/user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    用户管理导航，
+    <nuxt-link :to="'/user/101'">修改</nuxt-link>
+    <nuxt-child/>
+  </div>
+</template>
+<script>
+  export default {
+    layout: "test"
+  }
+</script>
+<style>
+
+</style>
